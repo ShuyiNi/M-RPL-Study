444fb047529567886b76902fcd52cec63eac16aa
qobilidop <qobilidop@gmail.com>
2020-02-27 12:21:58 -0800

    Recover confs
    Simulation results stay the same.

--- cooja-sim/project-conf.h
+++ cooja-sim/project-conf.h
@@ -4,9 +4,9 @@
 #define LOG_CONF_WITH_COMPACT_ADDR 1
 
 // /* Provisioning */
-// #define NETSTACK_MAX_ROUTE_ENTRIES 25
-// #define NBR_TABLE_CONF_MAX_NEIGHBORS 8
-// #define UIP_CONF_BUFFER_SIZE 512
+#define NETSTACK_MAX_ROUTE_ENTRIES 25
+#define NBR_TABLE_CONF_MAX_NEIGHBORS 8
+#define UIP_CONF_BUFFER_SIZE 512
 
 // /* RPL Objective Function */
 // #define RPL_CONF_OF_OCP RPL_OCP_OF0
