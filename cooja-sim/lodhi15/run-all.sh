5982dee48089e9ab05798ea1bed292330591e97f
qobilidop <qobilidop@gmail.com>
2020-03-13 14:03:18 -0700

    Correct parameters

--- cooja-sim/lodhi15/run-all.sh
+++ cooja-sim/lodhi15/run-all.sh
@@ -14,11 +14,11 @@ cooja-run -t mrpl-5-10 sim.csc
 cp Makefile.template Makefile
 sed -i 's/{{multipath}}/0/g' Makefile
 sed -i 's/{{n_src}}/14/g' Makefile
-sed -i 's/{{pkt_int}}/10/g' Makefile
+sed -i 's/{{pkt_int}}/5/g' Makefile
 cooja-run -t rpl-14-5 sim.csc
 
 cp Makefile.template Makefile
 sed -i 's/{{multipath}}/1/g' Makefile
 sed -i 's/{{n_src}}/14/g' Makefile
-sed -i 's/{{pkt_int}}/10/g' Makefile
+sed -i 's/{{pkt_int}}/5/g' Makefile
 cooja-run -t mrpl-14-5 sim.csc
