733fb087c83faf2464a92711031cc48af0aab3f6
liusw <liusw@pku.edu.cn>
2020-03-13 12:19:07 -0700

    save json in data analysis

--- bin/parse_log.py
+++ bin/parse_log.py
@@ -5,6 +5,7 @@ from collections import defaultdict
 import re
 import matplotlib.pyplot as plt
 import os
+import json
 
 
 def time_to_int(time_str):
@@ -73,21 +74,21 @@ def parse_file(file):
                 recv_items[(res['src'], res['dest'], res['id'], res['type'])] = res['time']
 
     return send_items, recv_items, all_json, err_line
-            
-            
-def draw_fig(file, base_dir='./'):
-    SMALL_NUM = 1e-20
+
+
+time_unit = 1_000_000 # unit in micro sec, 1 s = 1,000,000 ms
+SMALL_NUM = 1e-20
+
+def analyse_data(file, base_dir='./', file_prefix='', time_interval_secs=[0.5, 1, 1.5, 2, 5, 10], draw_fig=True, save_json=True):
     # file = 'log-20200227-201756/COOJA.testlog'
+    if file_prefix:
+        file_prefix = file_prefix.rstrip("_") + "_"
 
     parse_res = parse_file(os.path.join(base_dir, file))
-
-    time_unit = 1_000_000 # unit in micro sec, 1 s = 1,000,000 ms
-    time_interval = 5 * time_unit
-
     sends = parse_res[0]
     recvs = parse_res[1]
 
-    with open(os.path.join(base_dir, 'log_err_app_lines.txt'), 'w') as fw:
+    with open(os.path.join(base_dir, '%slog_err_app_lines.txt' % file_prefix), 'w') as fw:
         fw.write(parse_res[3])
 
     latency_count = []
@@ -108,8 +109,18 @@ def draw_fig(file, base_dir='./'):
     parse_info += "average latency:        %.6f\n" % avg_latency
     parse_info += "average delivery ratio: %.6f\n" % avg_deliver_rate
 
-    with open(os.path.join(base_dir, 'parse_info.txt'), 'w') as fw:
+    with open(os.path.join(base_dir, '%sparse_info.txt' % file_prefix), 'w') as fw:
         fw.write(parse_info)
+
+    if save_json:
+        with open('%slog_data.json' % file_prefix, 'w') as fw:
+            json.dump(parse_res[2], fw, indent=2)
+
+    for time_interval_sec in time_interval_secs:
+        group_analysis(sends, recvs, time_interval_sec, base_dir, file_prefix, draw_fig, save_json)
+
+def group_analysis(sends, recvs, time_interval_sec, base_dir, file_prefix, draw_fig, save_json):
+    time_interval = int(time_interval_sec * time_unit)
 
     all_times = list(sends.values()) + list(recvs.values())
     all_times.sort()
@@ -119,9 +130,9 @@ def draw_fig(file, base_dir='./'):
     latency_count_time = defaultdict(list)
 
     for msg, send_time in sends.items():
+        index = send_time // time_interval
         if msg in recvs:
             recv_time = recvs[msg]
-            index = send_time // time_interval
             deliver_count_time[index].append(1)
             latency_count_time[index].append(recv_time - send_time)
         else:
@@ -137,44 +148,46 @@ def draw_fig(file, base_dir='./'):
             throughput_count_time[index] += 1
     throughput_time = [throughput_count_time[i] / (time_interval / time_unit) for i in range(len(times))]
 
-    plt.figure(figsize=(15, 10))
-    plt.plot([i * time_interval / time_unit for i in range(len(times))], deliver_time, 'black', label='deliver rate')
-    plt.xlabel('time (s)', family='Times New Roman', fontsize=40)
-    plt.ylabel('packet delivery ratio', family='Times New Roman', fontsize=40)
-    plt.xticks(family='Times New Roman', fontsize=40)
-    plt.yticks(family='Times New Roman', fontsize=40)
-    plt.savefig(os.path.join(base_dir, 'deliverrate_t.png'))
-    plt.close()
+    if save_json:
+        deliverrate_t = dict(zip([i * time_interval / time_unit for i in range(len(times))], deliver_time))
+        with open (os.path.join(base_dir, '%sdeliverrate_ti=%ss.json' % (file_prefix, time_interval_sec)), 'w') as fw:
+            json.dump(deliverrate_t, fw, indent=2)
 
-    plt.figure(figsize=(15, 10))
-    plt.plot([i * time_interval / time_unit for i in range(len(times))], latency_time, 'black', label='latency')
-    plt.xlabel('time (s)', family='Times New Roman', fontsize=40)
-    plt.ylabel('latency (s)', family='Times New Roman', fontsize=40)
-    plt.xticks(family='Times New Roman', fontsize=40)
-    plt.yticks(family='Times New Roman', fontsize=40)
-    plt.savefig(os.path.join(base_dir, 'latency_t.png'))
-    plt.close()
+        latency_t = dict(zip([i * time_interval / time_unit for i in range(len(times))], latency_time))
+        with open (os.path.join(base_dir, '%slatency_ti=%ss.json' % (file_prefix, time_interval_sec)), 'w') as fw:
+            json.dump(latency_t, fw, indent=2)
 
-    plt.figure(figsize=(15, 10))
-    plt.plot([i * time_interval / time_unit for i in range(len(times))], throughput_time, 'black', label='throughput')
-    plt.xlabel('time (s)', family='Times New Roman', fontsize=40)
-    plt.ylabel('throughput (packets/s)', family='Times New Roman', fontsize=40)
-    plt.xticks(family='Times New Roman', fontsize=40)
-    plt.yticks(family='Times New Roman', fontsize=40)
-    plt.savefig(os.path.join(base_dir, 'throughput_t.png'))
-    plt.close()
+        throughput_t = dict(zip([i * time_interval / time_unit for i in range(len(times))], throughput_time))
+        with open (os.path.join(base_dir, '%sthroughput_ti=%ss.json' % (file_prefix, time_interval_sec)), 'w') as fw:
+            json.dump(throughput_t, fw, indent=2)
 
-    '''
-    l1=plt.plot(x1,y1,'r--',label='type1')
-    l2=plt.plot(x2,y2,'g--',label='type2')
-    l3=plt.plot(x3,y3,'b--',label='type3')
-    plt.plot(x1,y1,'ro-',x2,y2,'g+-',x3,y3,'b^-')
-    plt.title('The Lasers in Three Conditions')
-    plt.xlabel('row')
-    plt.ylabel('column')
-    plt.legend()
-    plt.show()
-    '''
+    if draw_fig:
+        plt.figure(figsize=(15, 10))
+        plt.plot([i * time_interval / time_unit for i in range(len(times))], deliver_time, 'black', label='deliver rate')
+        plt.xlabel('time (s)', family='Times New Roman', fontsize=40)
+        plt.ylabel('packet delivery ratio', family='Times New Roman', fontsize=40)
+        plt.xticks(family='Times New Roman', fontsize=40)
+        plt.yticks(family='Times New Roman', fontsize=40)
+        plt.savefig(os.path.join(base_dir, '%sdeliverrate_ti=%ss.png' % (file_prefix, time_interval_sec)))
+        plt.close()
+
+        plt.figure(figsize=(15, 10))
+        plt.plot([i * time_interval / time_unit for i in range(len(times))], latency_time, 'black', label='latency')
+        plt.xlabel('time (s)', family='Times New Roman', fontsize=40)
+        plt.ylabel('latency (s)', family='Times New Roman', fontsize=40)
+        plt.xticks(family='Times New Roman', fontsize=40)
+        plt.yticks(family='Times New Roman', fontsize=40)
+        plt.savefig(os.path.join(base_dir, '%slatency_ti=%ss.png' % (file_prefix, time_interval_sec)))
+        plt.close()
+
+        plt.figure(figsize=(15, 10))
+        plt.plot([i * time_interval / time_unit for i in range(len(times))], throughput_time, 'black', label='throughput')
+        plt.xlabel('time (s)', family='Times New Roman', fontsize=40)
+        plt.ylabel('throughput (packets/s)', family='Times New Roman', fontsize=40)
+        plt.xticks(family='Times New Roman', fontsize=40)
+        plt.yticks(family='Times New Roman', fontsize=40)
+        plt.savefig(os.path.join(base_dir, '%sthroughput_ti=%ss.png' % (file_prefix, time_interval_sec)))
+        plt.close()
 
 if __name__ == '__main__':
-    draw_fig('COOJA.testlog')
+    analyse_data('COOJA.testlog')
