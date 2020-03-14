daf01406ad149f0b3ce8d3851f526f6a69b614f8
qobilidop <qobilidop@gmail.com>
2020-03-13 17:07:51 -0700

    Small tweaks

--- cooja-sim/lodhi15/run-all.sh
+++ cooja-sim/lodhi15/run-all.sh
@@ -1,15 +1,4 @@
 #!/usr/bin/env bash
-cp Makefile.template Makefile
-sed -i 's/{{multipath}}/0/g' Makefile
-sed -i 's/{{n_src}}/5/g' Makefile
-sed -i 's/{{pkt_int}}/10/g' Makefile
-cooja-run -t rpl-5-10 sim.csc
-
-cp Makefile.template Makefile
-sed -i 's/{{multipath}}/1/g' Makefile
-sed -i 's/{{n_src}}/5/g' Makefile
-sed -i 's/{{pkt_int}}/10/g' Makefile
-cooja-run -t mrpl-5-10 sim.csc
 
 cp Makefile.template Makefile
 sed -i 's/{{multipath}}/0/g' Makefile
@@ -22,3 +11,15 @@ sed -i 's/{{multipath}}/1/g' Makefile
 sed -i 's/{{n_src}}/14/g' Makefile
 sed -i 's/{{pkt_int}}/5/g' Makefile
 cooja-run -t mrpl-14-5 sim.csc
+
+cp Makefile.template Makefile
+sed -i 's/{{multipath}}/0/g' Makefile
+sed -i 's/{{n_src}}/5/g' Makefile
+sed -i 's/{{pkt_int}}/10/g' Makefile
+cooja-run -t rpl-5-10 sim.csc
+
+cp Makefile.template Makefile
+sed -i 's/{{multipath}}/1/g' Makefile
+sed -i 's/{{n_src}}/5/g' Makefile
+sed -i 's/{{pkt_int}}/10/g' Makefile
+cooja-run -t mrpl-5-10 sim.csc
--- cooja-sim/lodhi15/sim.js
+++ cooja-sim/lodhi15/sim.js
@@ -1,4 +1,4 @@
-TIMEOUT(600000); /* Time out after 10 minutes */
+TIMEOUT(720000); /* Time out after 12 minutes */
 
 while(true) {
   log.log(time + "\t" + "ID:" + id + "\t"+ msg + "\n");
