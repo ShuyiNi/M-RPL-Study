6d50f08c3f0f9d2cd69fa6c7408f96af87f3b658
qobilidop <qobilidop@gmail.com>
2020-03-14 20:42:57 -0700

    Update Tang+16 experiment result

--- experiment/tang16/run-all.sh
+++ experiment/tang16/run-all.sh
@@ -2,13 +2,13 @@
 
 cp Makefile.template Makefile
 sed -i 's/{{multipath}}/0/g' Makefile
-sed -i 's/{{pkt_int}}/5/g' Makefile
+sed -i 's/{{pkt_int}}/4/g' Makefile
 make distclean
 cooja-run -t rpl sim.csc
 
 cp Makefile.template Makefile
 sed -i 's/{{multipath}}/1/g' Makefile
-sed -i 's/{{pkt_int}}/5/g' Makefile
+sed -i 's/{{pkt_int}}/4/g' Makefile
 make distclean
 cooja-run -t mrpl sim.csc
 
